ce91d64aef2ba6ce72b3cbd9eb86bae69cec48b7
Haus <reid@wilden.haus>
2022-05-10 23:20:14 -0400

    Fix scale when exporting. Additional buffer types support

--- H2ARipper/Converters/TplToFbxConverter.cs
+++ H2ARipper/Converters/TplToFbxConverter.cs
@@ -1,5 +1,6 @@
 ﻿using Aspose.ThreeD;
 using Aspose.ThreeD.Entities;
+using Aspose.ThreeD.Utilities;
 using LibH2A.Common;
 using LibH2A.Saber3D;
 
@@ -33,6 +34,7 @@ namespace H2ARipper.Converters
       var meshNode = new Node( "", mesh );
       scene.RootNode.AddChildNode( meshNode );
 
+      var uvs = mesh.CreateElementUV( TextureMapping.Diffuse, MappingMode.ControlPoint, ReferenceMode.Direct );
       foreach ( var bufInfo in meshData )
       {
         var buffer = tpl.Geometry.Data.Buffers[ ( int ) bufInfo.BufferId ];
@@ -62,31 +64,84 @@ namespace H2ARipper.Converters
           case S3D_GeometryData.S3D_BufferType.Unk_Face:
             reader.Seek( startOffset + ( submesh.FaceOffset * buffer.ElementSize ), SeekOrigin.Begin );
             for ( var i = 0; i < submesh.FaceCount; i++ )
-              mesh.CreatePolygon( reader.ReadInt16(), reader.ReadInt16(), 0 );
+              _ = reader.ReadInt32(); // Unk: MaterialID?
             break;
-          case S3D_GeometryData.S3D_BufferType.UvAndUnk:
+          case S3D_GeometryData.S3D_BufferType.StaticVert:
             reader.Seek( startOffset + ( submesh.VertOffset * buffer.ElementSize ), SeekOrigin.Begin );
-            var uvs = mesh.CreateElementUV( TextureMapping.Diffuse, MappingMode.ControlPoint, ReferenceMode.Direct );
             for ( var i = 0; i < submesh.VertCount; i++ )
             {
-              var unk = reader.ReadInt32(); // Blend Indices?
-
-              var u = IntToDouble( reader.ReadInt16() );
-              var v = 1 - IntToDouble( reader.ReadInt16() );
-              uvs.Data.Add( new Aspose.ThreeD.Utilities.Vector4( u, v, 0, 0 ) );
+              var x = SNorm( reader.ReadInt16() );
+              var y = SNorm( reader.ReadInt16() );
+              var z = SNorm( reader.ReadInt16() );
+              var w = SNorm( reader.ReadInt16() );
+              mesh.ControlPoints.Add( new Aspose.ThreeD.Utilities.Vector4( x, y, z, w ) );
             }
-            break;//Broken, probably a different structure
-          case S3D_GeometryData.S3D_BufferType.StaticVert:
+            break;
           case S3D_GeometryData.S3D_BufferType.SkinnedVert:
             reader.Seek( startOffset + ( submesh.VertOffset * buffer.ElementSize ), SeekOrigin.Begin );
             for ( var i = 0; i < submesh.VertCount; i++ )
             {
-              var x = IntToDouble( reader.ReadInt16() );
-              var y = IntToDouble( reader.ReadInt16() );
-              var z = IntToDouble( reader.ReadInt16() );
-              var w = IntToDouble( reader.ReadInt16() );
+              var x = SNorm( reader.ReadInt16() );
+              var y = SNorm( reader.ReadInt16() );
+              var z = SNorm( reader.ReadInt16() );
+              var w = SNorm( reader.ReadInt16() );
+
+              var weight1 = reader.ReadByte();
+              var weight2 = reader.ReadByte();
+              var weight3 = reader.ReadByte();
+              var weight4 = reader.ReadByte();
+
+              var bone1 = reader.ReadByte();
+              var bone2 = reader.ReadByte();
+              var bone3 = reader.ReadByte();
+              var bone4 = reader.ReadByte();
+
               mesh.ControlPoints.Add( new Aspose.ThreeD.Utilities.Vector4( x, y, z, w ) );
-              reader.BaseStream.Position += buffer.ElementSize - 8;
+            }
+            break;
+          case S3D_GeometryData.S3D_BufferType.UvAndUnk:
+            reader.Seek( startOffset + ( submesh.VertOffset * buffer.ElementSize ), SeekOrigin.Begin );
+            for ( var i = 0; i < submesh.VertCount; i++ )
+            {
+              var unk = reader.ReadInt32(); // Blend Indices?
+
+              var u = SNorm( reader.ReadInt16() );
+              var v = 1 - SNorm( reader.ReadInt16() );
+              uvs.Data.Add( new Aspose.ThreeD.Utilities.Vector4( u, v, 0, 0 ) );
+            }
+            break;
+          case S3D_GeometryData.S3D_BufferType.Unk_Vert30:
+            reader.Seek( startOffset + ( submesh.VertOffset * buffer.ElementSize ), SeekOrigin.Begin );
+            for ( var i = 0; i < submesh.VertCount; i++ )
+            {
+              _ = reader.ReadInt32(); // Unk: Normals?
+              _ = reader.ReadInt32(); // Unk: Binormals?
+              var u1 = SNorm( reader.ReadInt16() );
+              var v1 = 1 - SNorm( reader.ReadInt16() );
+              var u2 = SNorm( reader.ReadInt16() );
+              var v2 = 1 - SNorm( reader.ReadInt16() );
+
+              uvs.Data.Add( new Aspose.ThreeD.Utilities.Vector4( u1, v1, 0, 0 ) );
+              uvs.Data.Add( new Aspose.ThreeD.Utilities.Vector4( u2, v2, 0, 0 ) );
+            }
+            break;
+          case S3D_GeometryData.S3D_BufferType.Unk_Vert70:
+            reader.Seek( startOffset + ( submesh.VertOffset * buffer.ElementSize ), SeekOrigin.Begin );
+            for ( var i = 0; i < submesh.VertCount; i++ )
+            {
+              _ = reader.ReadInt32(); // Unk: Normals?
+              _ = reader.ReadInt32(); // Unk: Binormals?
+              _ = reader.ReadInt32(); // Unk: Tangents?
+              var u1 = SNorm( reader.ReadInt16() );
+              var v1 = 1 - SNorm( reader.ReadInt16() );
+              var u2 = SNorm( reader.ReadInt16() );
+              var v2 = 1 - SNorm( reader.ReadInt16() );
+              var u3 = SNorm( reader.ReadInt16() );
+              var v3 = 1 - SNorm( reader.ReadInt16() );
+
+              uvs.Data.Add( new Aspose.ThreeD.Utilities.Vector4( u1, v1, 0, 0 ) );
+              uvs.Data.Add( new Aspose.ThreeD.Utilities.Vector4( u2, v2, 0, 0 ) );
+              uvs.Data.Add( new Aspose.ThreeD.Utilities.Vector4( u3, v3, 0, 0 ) );
             }
             break;
           default:
@@ -95,12 +150,25 @@ namespace H2ARipper.Converters
         }
       }
 
+      // Transforms
+      var pos = submesh.TransformInfo.Position;
+      var posX = SNorm( pos[ 0 ] );
+      var posY = SNorm( pos[ 1 ] );
+      var posZ = SNorm( pos[ 2 ] );
+      meshNode.Transform.Translation = new Vector3( posX, posY, posZ );
+
+      var scale = submesh.TransformInfo.Scale;
+      var scaleX = scale[ 0 ];
+      var scaleY = scale[ 1 ];
+      var scaleZ = scale[ 2 ];
+      meshNode.Transform.Scale = new Vector3( scaleX, scaleY, scaleZ );
+
     }
 
-    private static double IntToDouble( in short value )
+    private static double SNorm( in short value )
       => ( double ) value / short.MaxValue;
 
-    private static double IntToDouble( in ushort value )
+    private static double UNorm( in ushort value )
       => ( double ) value / ushort.MaxValue;
 
   }
